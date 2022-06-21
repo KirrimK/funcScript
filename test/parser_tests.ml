f4b5b1e1f59969819ae074d04874900c5b2d22cd
KirrimK <KirrimK@users.noreply.github.com>
2022-06-22 00:28:35 +0200

    more parsing tests

--- test/parser_tests.ml
+++ test/parser_tests.ml
@@ -10,9 +10,21 @@ let generic_parse_test = fun test_name to_parse expected ()->
   with e ->
     (test_name, Error(Printexc.to_string e))
 
-let test_one_semicolon = generic_parse_test
-  "one_semicolon" ";" STAT_NOOP
+let test_stat_noop = generic_parse_test
+  "stat_noop" "" STAT_NOOP
+
+let test_stat_pause = generic_parse_test
+  "stat_pause" "pause 0; #" (STAT_PAUSE(EXPR_LITERAL(LITERAL_INT(0)), STAT_EXPR(EXPR_LITERAL(LITERAL_NONE))))
+
+let test_stat_expr = generic_parse_test
+  "stat_expr" "0" (STAT_EXPR(EXPR_LITERAL(LITERAL_INT(0))))
+
+  let test_stat_assign = generic_parse_test
+  "stat_assign" "a = 0 in a" (STAT_ASSIGN([EXPR_IDENTIFIER("a")], [EXPR_LITERAL(LITERAL_INT(0))], STAT_EXPR(EXPR_IDENTIFIER("a"))))
 
 let () = run_tests_and_display "PARSING" [
-  test_one_semicolon;
+  test_stat_noop;
+  test_stat_pause;
+  test_stat_expr;
+  test_stat_assign;
 ]
