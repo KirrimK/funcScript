710d6aee3cb8e20767a488a712f7184c0fd66b2e
Rémy B <KirrimK@users.noreply.github.com>
2022-07-05 11:47:41 +0000

    added nix packages to allow replit to build

--- replit.nix
+++ replit.nix
@@ -4,6 +4,7 @@
         pkgs.dune_2
         pkgs.ocamlPackages.menhir
         pkgs.ocamlPackages.bisect_ppx
+        pkgs.ocamlPackages.findlib
         pkgs.ocamlPackages.dune-build-info
     ];
 }
