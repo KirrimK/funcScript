ae573c5b5ebd1a9f189ebda125705dc07ae0f2a2
Rémy B <KirrimK@users.noreply.github.com>
2022-07-05 11:56:19 +0000

    added nix pkg dependency

--- replit.nix
+++ replit.nix
@@ -3,6 +3,7 @@
         pkgs.ocaml
         pkgs.dune_2
         pkgs.ocamlPackages.menhir
+        pkgs.ocamlPackages.ppxlib
         pkgs.ocamlPackages.bisect_ppx
         pkgs.ocamlPackages.findlib
         pkgs.ocamlPackages.dune-build-info
